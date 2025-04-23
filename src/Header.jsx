891fd9016735c09f38731f3feb257e2b1905a9c2
Benoit Thery <thery.benoit@hotmail.fr>
2025-04-23 15:56:00 +0200

    Intégration React

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header () {
+return (<header>
+  <div>Benoit Théry, développeur frontend</div>
+  <div>
+    <p>Mon portfolio</p>
+    <div>MoovIt</div>
+    <div>Kaza</div>
+    <div>CaesR</div>
+  </div>
+</header>)
+}
+
+export default Header
