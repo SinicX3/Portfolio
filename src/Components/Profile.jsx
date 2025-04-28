f26c17412fb7a83fa8b2f8347ecdc3c0f10bc802
Benoit Thery <thery.benoit@hotmail.fr>
2025-04-28 18:08:34 +0200

    Ajouts sur la Home

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -6,13 +6,36 @@ function Profile () {
       <img src="/IMG_9743.jpg" id="photo"/>
       <div className="text">
         <h1>Développeur frontend</h1>
-        <p>Formé à React, j'ai une appétence pour les interfaces intuitives et agréables.</p>
+        <p>Formé à JavaScript (avec un intérêt prononcé pour React) j'ai une appétence pour les interfaces intuitives et agréables.</p> 
       </div>
     </div>
     <div className="skills">
-      <div>Compétence 1</div>
-      <div>Compétence 2</div>
-      <div>Compétence 3</div>
+      <div>
+        <img src='/React-icon.png' id="react"/>
+        <p>Formé à React</p> 
+        <ul>
+          <li>Maîtrise des concepts de composants, props, hooks et états</li>
+          <li>Egalement des bases en backend (Express, Node, MongoDB...)</li>
+        </ul>
+      </div>
+      <div>
+        <img src='SEO_logo.png' id="SEO"/>
+        <p>Excellent rédactionnel</p> 
+        <ul>
+          <li>10 ans d'expérience comme rédacteur web</li>
+          <li>Excellent niveau de français</li>
+          <li>Bon niveau d'anglais</li> 
+          <li>Expérience du SEO</li>
+        </ul>
+      </div>
+      <div>
+        <img src='SoftSkills_logo.png' id="soft"/>
+        <p>Soft skills adaptés au travail d'équipe</p>
+        <ul>
+          <li>Profil enthousiaste et collaboratif</li>
+          <li>Flexibilité de travail</li>
+        </ul>
+      </div>
     </div>
   </section>)
 }
